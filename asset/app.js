907f5fd8ff67c0ed6a5385531fb1cc5ec4b4d40e
Amit kumar <amit0056kr@gmail.com>
2022-01-30 10:28:55 +0530

    music website

---
+++ asset/app.js
@@ -0,0 +1,11 @@
+// alert("welcome to Music Hub");
+
+var x = document.getElementById("myAudio"); 
+      
+      function playAudio() { 
+        x.play(); 
+      } 
+      
+      function pauseAudio() { 
+        x.pause(); 
+      } 
